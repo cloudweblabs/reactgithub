7ea96a88d0201e25861841e1b204aae4bd680335
test cloud <test.cloudweblabs@gmail.com>
2019-07-29 10:35:38 +0530

    Updates

---
+++ precache-manifest.b904b160e131c6c301e835967d913521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5dae8d7d536763f4bd5abe268baa5fd",
+    "url": "/reactgithub/index.html"
+  },
+  {
+    "revision": "5067291452f5193e27d8",
+    "url": "/reactgithub/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "08d74e87ba1b61c7a802",
+    "url": "/reactgithub/static/css/main.6a5f8963.chunk.css"
+  },
+  {
+    "revision": "5067291452f5193e27d8",
+    "url": "/reactgithub/static/js/2.923c19b6.chunk.js"
+  },
+  {
+    "revision": "08d74e87ba1b61c7a802",
+    "url": "/reactgithub/static/js/main.6894c56e.chunk.js"
+  },
+  {
+    "revision": "37e05e5a7934c7a39a24",
+    "url": "/reactgithub/static/js/runtime~main.00ba7cee.js"
+  },
+  {
+    "revision": "3f2c979b214d06e9caab8ba8326864f3",
+    "url": "/reactgithub/static/media/loading.3f2c979b.gif"
+  }
+]);
